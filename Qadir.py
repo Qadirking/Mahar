295e4991f3516e6ebafcf31f7224c280ef70f518
Muhammad Qadir <87000895+Qadirking@users.noreply.github.com>
2022-04-22 22:54:39 +0500

    Update Qadir.py

--- Qadir.py
+++ Qadir.py
@@ -1,2 +1,16 @@
-from Qadir import knock_Out
-knock_Out()
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Qadir import knock_Out
+    knock_Out()
+elif bit == '32bit':
+    from Qdr import knock_Out
+    knock_Out()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.sys.exit()
