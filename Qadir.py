f4220a3ed87ea2adc142c12f01ffd4465819d3be
Muhammad Qadir <87000895+Qadirking@users.noreply.github.com>
2022-04-28 19:55:57 +0500

    Update Qadir.py

--- Qadir.py
+++ Qadir.py
@@ -9,7 +9,7 @@ if bit == '64bit':
     from Qadir import knock_Out
     knock_Out()
 elif bit == '32bit':
-    from Qdr import knock_Out
+    from Qadir32 import knock_Out
     knock_Out()
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
